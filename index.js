47dccf98330acca1534581488b4e2498b6b8eb9c
Ryan Inawat <rrinawat@gmail.com>
2017-12-06 17:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ function getFirstSelector(selector){
 function nestedTarget(){
    return document.querySelector('#nested .target')
 }
-//iterate down to the deepest child
+//iterate down to the deepest child...brute force version
 function deepestChild(){
    return  document.querySelector('#grand-node div div div div')
+}
+// randklist iteration
+function increaseRankBy(n){
+  const ranks=document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < ranks.length; i++) {
+  ranks[i].innerHTML = (i + n).toString()
+}
 }
