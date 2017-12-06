d50b124406c950e809fc8ae7ca7a8a2672e72f48
Ryan Inawat <rrinawat@gmail.com>
2017-12-06 17:38:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+//simple implementation of querySelector
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+//remember since nested in an id, can use # to select ID, then use .target to find that class name under that branch
+function nestedTarget(){
+   return document.querySelector('#nested .target')
+}
+//iterate down to the deepest child...brute force version
+function deepestChild(){
+   return  document.querySelector('#grand-node div div div div')
+}
+// randklist iteration
+function increaseRankBy(n){
+  const ranks=document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < ranks.length; i++) {
+  ranks[i].innerHTML = (i + n).toString()
+  }
+}
