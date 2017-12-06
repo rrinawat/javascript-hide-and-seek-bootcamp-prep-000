52cb723b156b39f7cfb84e0d8e0ddaefae66e9bd
Ryan Inawat <rrinawat@gmail.com>
2017-12-06 17:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function nestedTarget(){
 }
 //iterate down to the deepest child
 function deepestChild(){
-   return  document.querySelectorAll('#grand-node div div div div')
+   return  document.querySelectorAll('#grand-node div div div div').innerHTML
 }
